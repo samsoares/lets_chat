f263e38188e25fb2a5965e63a63eb0ccbd6b50e3
Samuel Soares <samjsoares@gmail.com>
2020-04-14 01:02:04 -0400

    inital chat

---
+++ server.js
@@ -0,0 +1,24 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg) {
+    console.log('message: ' + msg);
+    socket.broadcast.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
