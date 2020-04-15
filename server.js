7af4d488ddb15494542a1f55615e7b1146daad90
Samuel Soares <samjsoares@gmail.com>
2020-04-15 00:36:42 -0400

    add port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const PORT = (process.env.PORT || 3000);
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
@@ -24,6 +26,6 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(3000, function() {
-  console.log('listening on *:3000');
+http.listen(PORT, function() {
+  console.log('listening on ' + PORT);
 });
