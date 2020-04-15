735e673276e23fdd0f17bcb1ffd1941bd805af6b
Samuel Soares <samjsoares@gmail.com>
2020-04-15 00:23:28 -0400

    add routing for rooms

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/rooms/:roomId', function(req, res) {
+  console.log('Connected to room ' + req.params['roomId']);
+  res.sendFile(__dirname + '/room.html');
+});
+
 io.on('connection', function(socket) {
   console.log('a user connected');
   
