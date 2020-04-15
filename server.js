79232fd634b1d93e96657fdc3bb91b384f38d25e
Samuel Soares <samjsoares@gmail.com>
2020-04-15 00:32:24 -0400

    heroku support. update version

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/rooms/:roomId', function(req, res) {
 
 io.on('connection', function(socket) {
   console.log('a user connected');
-  
+
   socket.on('disconnect', function() {
     console.log('user disconnected');
   });
